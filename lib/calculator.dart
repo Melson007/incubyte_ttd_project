0fdb028969ac67ad89d4ec0670eec1a8a3475f95
Melson30 <108928140+Melson30@users.noreply.github.com>
2025-02-23 17:45:37 +0530

    Test 2 - Handle single and two numbers

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,8 +1,7 @@
 class StringCalculator {
   int add(String numbers) {
-    if (numbers.isEmpty) {
-      return 0;
-    }
-    return int.parse(numbers);
+    if (numbers.isEmpty) return 0;
+    List<String> numList = numbers.split(',');
+    return numList.map(int.parse).reduce((a, b) => a + b);
   }
 }
--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -6,4 +6,14 @@ void main() {
     final calculator = StringCalculator();
     expect(calculator.add(""), 0);
   });
+
+  test('single number should return the number itself', () {
+    final calculator = StringCalculator();
+    expect(calculator.add("1"), 1);
+  });
+
+  test('two numbers separated by a comma should return their sum', () {
+    final calculator = StringCalculator();
+    expect(calculator.add("1,2"), 3);
+  });
 }
